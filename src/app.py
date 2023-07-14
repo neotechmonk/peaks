496a2deaf42e83418889853d2f8f780ac4c93580
Neo Tech Monk <your_email@gmail.com>
2023-07-15 02:33:46 +1000

    Styles as dataclasses

--- src/app.py
+++ src/app.py
@@ -1,14 +1,19 @@
 from functools import cache
 
-from chart import draw_chart
+from chart import (DownMarkerStyle, LineStyle, UpMarkerStyle,
+                   create_additional_plot, draw_chart)
 from data import sample_6M_daily_price
 
 
 def main():
-    
-    data= sample_6M_daily_price(ticker="AAPL")
-    # print(data)
-    fig = draw_chart(data)
+    data = sample_6M_daily_price(ticker="AAPL")
+    add_plotdata = data["Close"] * 2
+
+    line_style = UpMarkerStyle(ylabel='signal')
+
+    add_plot_input = (add_plotdata, line_style)
+
+    fig = draw_chart(data, create_additional_plot(add_plot_input))
     fig.show()
 
 if __name__ =="__main__":
--- src/chart.py
+++ src/chart.py
@@ -1,21 +1,66 @@
 
-from typing import Any, Dict, List
+from dataclasses import dataclass, field
+from typing import Any, Callable, Dict, List, TypeVar
 
 import mplfinance as mpf
+import numpy as np
 import pandas as pd
 
+PriceData = TypeVar('PriceData', np.ndarray, List, pd.DataFrame)
 
-def draw_chart(data: pd.DataFrame, addplots: List[Dict[str, Any]] = None, plot_settings: Dict[str, Any] = None) -> Any:
+
+def draw_chart(data: PriceData, 
+               addplots: List[Dict[str, Any]] = None, 
+               plot_settings: Dict[str, Any] = None, 
+               returnfig = False) -> Any:
     """
     Function to draw a chart with optional inputs
         - additional plots (as a list of dictionaries)
         - plot_settings
+
+    Params: 
+        @returnfig = returns the fig instead of plotting/showing the chart 
     """
     plot_params = plot_settings.copy() if plot_settings else {}
 
     if addplots is not None:
         plot_params["addplot"] = addplots
 
-    fig, _ = mpf.plot(data=data, **plot_params)
-
+    fig,_ = mpf.plot(data=data, **plot_params,returnfig=returnfig)
+    
     return fig
+
+
+@dataclass  
+class PlotStyle:
+    ylabel: str 
+    type:str
+    color:str
+
+@dataclass
+class LineStyle(PlotStyle):
+    type: str = 'line'
+    color: str = 'blue'
+    
+
+@dataclass
+class DownMarkerStyle(PlotStyle):
+    type: str = 'scatter'
+    color: str = 'red'
+    marker: str =  'v'
+    markersize : int = 50 
+
+@dataclass
+class UpMarkerStyle(PlotStyle):
+    type: str = 'scatter'
+    color: str = 'green'
+    marker: str =  '^'
+    markersize : int = 50 
+
+
+
+AdditionalPlotInput = Callable[[PriceData, PlotStyle], Dict[str, Any]]
+
+def create_additional_plot(add_plot_input: AdditionalPlotInput) -> Any:
+    data, plot_style = add_plot_input
+    return mpf.make_addplot(data, **plot_style.__dict__)

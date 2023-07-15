1585496b3ce8d8e50bfc055d7b7c49ba15c05faf
Neo Tech Monk <your_email@gmail.com>
2023-07-15 12:55:36 +1000

    refactor peaks module to a single file for all types of peak finders

--- src/app.py
+++ src/app.py
@@ -1,26 +1,30 @@
 
 
+import numpy as np
+
 from chart import PLOT_STYLE, additional_plot_factory, draw_chart
 from data import sample_6M_daily_price
-from lib.scipy_signal_find_peaks_cwt import find_peaks
+from lib.peaks import scipy_signal_find_peaks_cwt
 
 
 def draw_peaks(ticker: str, peak_finder_fn, *peak_finder_fn_args, **peak_finder_fn_kwargs):
-    data = sample_6M_daily_price(ticker="AAPL")
+    data = sample_6M_daily_price(ticker=ticker)
     addplots =[]
 
     scipy_cwt_peaks = peak_finder_fn(data['High'], *peak_finder_fn_args, **peak_finder_fn_kwargs)
     addplots.append(additional_plot_factory(additional_plot_data=scipy_cwt_peaks,
-                                             additional_plot_style=PLOT_STYLE.DOWN_MARKER,
-                                             label="scipy_cwt_peaks"))
+                                             additional_plot_style=PLOT_STYLE.DOWN_MARKER
+                                            ))
 
 
-    fig = draw_chart(data, addplots, {'title':f"{ticker} -{peak_finder_fn.__module__}.{peak_finder_fn.__name__}"}, returnfig=False)
+    fig = draw_chart(data, addplots, {'title':f"{ticker} - {peak_finder_fn.__module__}.{peak_finder_fn.__name__}"}, returnfig=False)
     fig.show()
 
 
 def main():
-    draw_peaks(ticker='MSFT', peak_finder_fn=find_peaks, widths=None, max_distances=None)
+    ticker= 'MSFT'
+
+    draw_peaks(ticker=ticker, peak_finder_fn=scipy_signal_find_peaks_cwt, widths=np.arange(1,4), max_distances=np.arange(1,4))
     
 
 if __name__ =="__main__":
